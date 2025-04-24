d919857cd7d58b03c6ba8e6eb98f028113536ee4
SerginoLimoncino <sergiotassoni70@proton.me>
2025-04-24 20:39:00 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>PixelHype è vivo e online! 🚀</h1>
+    </div>
+  );
+}
