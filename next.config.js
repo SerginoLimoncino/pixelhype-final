995b3ed54059b8a5bec633747cd51bc70a4324cf
SerginoLimoncino <sergiotassoni70@proton.me>
2025-04-25 18:46:08 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
-module.exports = { reactStrictMode: true };
+module.exports = {
+  reactStrictMode: true,
+  webpack(config, { dev }) {
+    if (dev) {
+      // Disabilita la minificazione solo durante lo sviluppo
+      config.optimization.minimize = false;
+    }
+    return config;
+  },
+}
