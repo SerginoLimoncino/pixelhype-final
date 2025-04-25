6e635763e9bf3659ba2848e2cccdd58397444285
SerginoLimoncino <sergiotassoni70@proton.me>
2025-04-25 04:28:12 +0200

    Create pixels.jsx

---
+++ pages/pages/pixels.jsx
@@ -0,0 +1,12 @@
+import PixelGrid from "../components/PixelGrid";
+
+export default function PixelsPage() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", margin: "20px 0" }}>
+        Mappa Interattiva PixelHype
+      </h1>
+      <PixelGrid />
+    </div>
+  );
+}
